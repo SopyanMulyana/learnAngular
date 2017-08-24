814882c19dd6059208994032af7ff37972a90688
Sopyan M <sopyan.mulyana@mitrais.com>
2017-08-24 11:43:53 +0800

    Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './componen.html', // here is template, its can be template url for you to create seperate html file
   styleUrls: ['./componenstyle.css']
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  show: boolean = true;
+
+  persons: any[] = [
+    {
+      ID: 1,
+      name: 'Sopyan Mulyana',
+      email: 'sopyan.mulyana@mitrais.com',
+      age: 23
+    },
+    {
+      ID: 2,
+      name: 'Daniel Muliawan',
+      email: 'daniel.muliawan@mitrais.com',
+      age: 33
+    },
+    {
+      ID: 3,
+      name: 'Wisesa Fentika',
+      email: 'wisessa.fentika@mitrais.com',
+      age: 31
+    }
+  ]
+}
